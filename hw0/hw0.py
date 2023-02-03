48d933d995c09b10e3892baa7b3193b52fde9070
Aidan Tai <aidantai100@gmail.com>
2023-02-02 23:36:24 -0500

    latex fixed, py fixed

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -16,18 +16,13 @@ def problem_two():
     X_1, X_2 = np.meshgrid(X_1, X_2)
     Y = np.exp(-(X_1-2)**2-(X_2-1)**2)
 
-    fig = plt.figure() #figsize=(6, 6)
-    ax = fig.add_subplot(111, projection='3d') #111 -> position
+    fig = plt.figure() 
+    ax = fig.add_subplot(111, projection='3d') 
     ax.plot_surface(X_1, X_2, Y, cmap=cm.coolwarm, linewidth=0, antialiased=False)
     plt.show()
 
 
 def problem_three_one():
-    # x = np.linspace(0, 240, 10)
-    # y = np.linspace(0, 10, 10)
-    # x, y = np.meshgrid(x, y)
-    # pos = np.dstack((x, y))
-
     mean = [120, 4]
     cov = [[1.5, 1], [1, 1.5]]
     rv = multivariate_normal(mean, cov)
@@ -43,10 +38,8 @@ def problem_three_one():
     df = pandas.DataFrame(data=data)
     seaborn.histplot(df, x="S", y="W")
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.contourf(x, y, rv.pdf(pos))
     plt.show()
+
 
 def problem_three_two():
     mean = [120, 4]
@@ -59,29 +52,18 @@ def problem_three_two():
         p_118.append(rv.pdf([118,w[i]]))
         p_122.append(rv.pdf([122,w[i]]))
 
-
-    # S = []
-    # W = []
-    # for sample in samples:
-    #     s, w = sample
-    #     S.append(s)
-    #     W.append(w)
-    # data = {"S":S, "W":W};
-    # df = pandas.DataFrame(data=data)
-    # seaborn.histplot(df, x="S", y="W")
-
-    # fig = plt.figure(1, 1)
     fig, ax = plt.subplots(1, 1, figsize=[15,5])
-    # ax = fig.add_subplot(111)
     ax.plot(w, p_118)
     ax.plot(w, p_122)
     plt.show()
+
 
 def total_processing_sample():
     packages = np.random.poisson(72)
     processes = np.random.normal(86.4, 5.84, size=packages)
     total_processing_time = np.sum(processes)
     return total_processing_time
+
 
 def problem_three_five():
     samples = []
@@ -94,7 +76,8 @@ def problem_three_five():
 
 
 if __name__ == '__main__':
-    # problem_three_one()
-    # problem_three_two()
+    problem_two()
+    problem_three_one()
+    problem_three_two()
     problem_three_five()
     
